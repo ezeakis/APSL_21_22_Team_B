e54154dbb686041caa6d9ca7d2000ff5f44991de
Manos Zeakis <ezeakis@gmail.com>
2021-12-30 18:43:35 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#We based this code from this lesson
+#https://projects.raspberrypi.org/en/projects/code-for-your-astro-pi-mission-space-lab-experiment/3
+
+import csv
+from sense_hat import SenseHat
+from datetime import datetime
+from pathlib import Path
+from time import sleep
+
+sense = SenseHat()
+
+base_folder = Path(__file__).parent.resolve()
+data_file = base_folder/'data.csv'
+
+with open(data_file, 'w', buffering=1) as f:
+    writer = csv.writer(f)
+    header = ("Date/time", "Temperature", "Humidity", "Pressure")
+    writer.writerow(header)
+    for i in range(10):
+        row = (datetime.now(), sense.temperature, sense.humidity, sense.pressure)
+        writer.writerow(row)
+        sleep(60)
